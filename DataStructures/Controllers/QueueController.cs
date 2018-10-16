e3429d8a09d1fb42af877b7d14744d5efc03fe1a
Kyle <kylebharrison16@gmail.com>
2018-10-15 23:38:25 -0600

    The updates for the Queue

--- DataStructures/Controllers/QueueController.cs
+++ DataStructures/Controllers/QueueController.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,9 +12,12 @@ namespace DataStructures.Controllers
         //declare variable
         //create new queue variable of type string
         static Queue<string> myQueue = new Queue<string>();
+        bool checkSearch = true;
+        private bool checkQueue;
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
 
 
-       
+
         public ActionResult Index()
         {
             //add this so that when page first loads we have something added to the queue
@@ -30,63 +33,103 @@ namespace DataStructures.Controllers
             return View("Index");
         }
 
-        //Action result that will add huge list of items to stack
+        //Action result that will add huge list of items to Queue
         public ActionResult HugeList()
         {
             //clears the queue
             myQueue.Clear();
 
-            //loop to add 2000 new items to the stack
-            for (int i = 0; i <= 2000; i++)
+            //loop to add 2000 new items to the Queue
+            for (int i = 0; i < 2000; i++)
             {
-                myStack.Push("New Entry " + (myStack.Count + 1));
+                myQueue.Enqueue("New Entry " + (myQueue.Count + 1));
             }
-            ViewBag.MyStack = myStack;
+
+            ViewBag.MyQueue = myQueue;
             return View("Index");
         }
 
-        //action result that will display the stack
-        public ActionResult DisplayStack()
+        //action result that will display the Queue
+        public ActionResult DisplayQueue()
         {
-            ViewBag.MyStack = myStack;
-            return View("DisplayStack");
+            ViewBag.MyQueue = myQueue;
+            return View("DisplayQueue");
         }
 
-        /*
-        //action result that will delete items from the stack
-        public ActionResult DeleteStack()
+
+        //action result that will delete items from the Queue
+        public ActionResult DeleteQueue()
         {
+            //use pop because it deletes most recent
+            if (myQueue.Count == 0)
+            {
+                return View("NO");
+            }
+            else
+            {
+                myQueue.Dequeue();
+                ViewBag.MyQueue = myQueue;
+                return View("Index");
+            }
+            
 
         }
-        */
 
-        //action result that will clear the stack
-        //public ActionResult ClearStack()
-       // {
-          //  myStack.Clear();
-          //  ViewBag.MyStack = myStack;
-          //  return View("Index");
-       // }
 
-        /*
-        //action result that will search the stack
-        public ActionResult SearchStack()
+        //action result that will clear the Queue
+        public ActionResult ClearQueue()
         {
-
+            myQueue.Clear();
+            ViewBag.MyQueue = myQueue;
+            return View("Index");
         }
-        */
 
-        /*action result to return to the main menu
-        public ActionResult Return()
+
+        //action result that will search the Queue
+        public ActionResult SearchQueue()
         {
-            return View()
-        }
-        */
-        /*
-        public ActionResult Index()
-        {
-            return View();
+
+            
+           
+            
+                //starts the stopwatch either way, only will if it is true
+                //starts the stopwatch
+                sw.Start();
+                //Searches the Queue for New Entry 5
+                checkQueue = myQueue.Contains("New Entry 5");
+
+                if (checkQueue == true)
+                {
+                    ViewBag.Output = "Found entry \"New Entry 5\"!";
+
+                    //stops the stopwatch
+                    sw.Stop();
+
+                    TimeSpan ts = sw.Elapsed;
+
+                    ViewBag.StopWatch = ts;
+                }
+
+                else
+                {
+                    ViewBag.Output = "\"New Entry 5\" Not Found :(";
+                }
+
+            
+
+
+            return View("SearchQueue");
+
+
+            /*action result to return to the main menu
+            public ActionResult Return()
+            {
+                return View()
+            }
+            */
+
+
+
         }
     }
 }
-*/
